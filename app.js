691147cd954a2fea6687c839251855fe5a855657
yassine9812 <yassine50.zayet@gmail.com>
2026-01-06 21:55:45 +0100

    API_ Handling GET Requests

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello, World!', app: 'My Express App' });
-});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
+);
 
-app.post('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'POST request received', app: 'My Express App' });
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
